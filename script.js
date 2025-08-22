7cca2e5857dca8a921604941804a9f4570c4c91f
Yoshida Shunsuke <koedoyoshida@gmail.com>
2025-08-23 00:17:21 +0900

    Add mute query search

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ var script01 = function()
 {
 	chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
 		var url = tabs[0].url;
+		var base_url = url.split("?")[0]
 		//alert(url);
-		chrome.tabs.create({'url': 'https://twitter.com/search?q=' + encodeURIComponent(url) + ''}, function(tab) {});
+		chrome.tabs.create({'url': 'https://twitter.com/search?q=' + encodeURIComponent(base_url) + "%20OR%20" + encodeURIComponent(url) }, function(tab) {});
 	});
 };
  
