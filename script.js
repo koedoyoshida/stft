77d76d85bdb971aaa8998cad7f7586613ff64875
Yoshida Shunsuke <koedoyoshida@gmail.com>
2025-08-22 23:56:56 +0900

    Fix typo

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var script01 = function()
 		var url = tabs[0].url;
 		//alert(url);
 		chrome.tabs.create({'url': 'https://twitter.com/search?q=' + encodeURIComponent(url) + ''}, function(tab) {});
-t(url) + '');
 	});
 };
  
